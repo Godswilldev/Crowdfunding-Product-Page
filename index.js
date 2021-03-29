6b5660ec6fcb5d8ddbacfadcbbea5532d4f1b7d8
Godswilldev <gimmex1@gmail.com>
2021-03-29 23:31:40 +0100

    complete header

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-let hamburger = document.querySelector('.hamburger')
-let navbar = document.querySelector('.navbar')
+let hamburger = document.querySelector(".hamburger");
+let navbar = document.querySelector(".navbar");
+let btn_primary = document.querySelector(".btn__primary");
+let overlay = document.querySelector("#overlay");
+let close_Modal = document.querySelector(".closeModal");
+let projectModal = document.querySelector(".projectModal");
+let pledge = document.querySelectorAll(".pledge");
+let projectModal__pledges = document.querySelector(".projectModal__pledges");
 
+hamburger.addEventListener("click", () => {
+  navbar.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
+});
+const openModal = () => {
+  overlay.classList.remove("hidden");
+  projectModal.classList.remove("hidden");
+};
+const closeModal = () => {
+  overlay.classList.add("hidden");
+  projectModal.classList.add("hidden");
+};
 
-hamburger.addEventListener('click', () => {
-    navbar.classList.toggle('hidden')
-})
+btn_primary.addEventListener("click", openModal);
+close_Modal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+///To remove the overlay when active
+overlay.addEventListener("click", () => {
+  navbar.classList.remove("hidden");
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key == "Escape") {
+    closeModal();
+  }
+});
